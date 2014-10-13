5652d0f2f724a0dc272610cf0f5d485a918ee5b9
Stanley Gu <stanleygu@gmail.com>
2014-10-13 01:24:00 +0000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='MODEL7743315447',
+      version=20140916,
+      description='MODEL7743315447 from BioModels',
+      url='http://www.ebi.ac.uk/biomodels-main/MODEL7743315447',
+      maintainer='Stanley Gu',
+      maintainer_url='stanleygu@gmail.com',
+      packages=find_packages(),
+      package_data={'': ['*.xml', 'README.md']},
+    )
